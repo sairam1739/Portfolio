4c8a526a5d832bab6a08a44dfb644231c0aca966
sairam1739 <andesairam1739@gmail.com>
2024-07-29 22:26:12 +0530

    my portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Navabar from './components/Navabar';
+import Home from './components/Home'
+import Skills from './components/Skills';
+import Projects from './components/Projects'
+import Contact from './components/Contact';
+import Aos from 'aos';
+import "aos/dist/aos.css"
+import { useEffect } from 'react';
 
-function App() {
+function  App() {
+  useEffect(() => {
+   Aos.init()
+  
+  
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+     <Navabar/>
+     <div className='container'>
+      <Home/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+     </div>
+    </>
   );
 }
 
